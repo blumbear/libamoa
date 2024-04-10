9caa53ff94a344ed8378e9d5978e22e5363fe03f
Tom Taquet <ttaquet@c1r2p1.42lehavre.fr>
2024-04-10 18:49:00 +0200

    ~ | update intlen to clean result when it is negative number

--- src/STRING/ft_intlen.c
+++ src/STRING/ft_intlen.c
@@ -6,7 +6,7 @@
 /*   By: ttaquet <ttaquet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 13:59:21 by ttaquet           #+#    #+#             */
-/*   Updated: 2024/03/21 14:16:11 by ttaquet          ###   ########.fr       */
+/*   Updated: 2024/04/10 18:47:33 by ttaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,16 @@
 size_t	ft_intlen(int number)
 {
 	size_t	res;
+	int		sign;
 
 	res = 0;
-	while (number > 0)
+	sign = (number > 0);
+	if (number == 0)
+		return (1);
+	while (number * ((sign * 2) - 1) > 0)
 	{
 		number = number / 10;
 		res++;
 	}
-	return (res);
+	return (res + (sign == 0));
 }
